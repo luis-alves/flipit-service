8bea85fd780eecad9cbc27e27d83034cf073b22f
Luís Alves <lmoalves@gmail.com>
2021-06-26 22:21:31 +0100

    Create user entity

--- src/main/java/io/amonite/flipitservice/domain/model/entities/User.java
+++ src/main/java/io/amonite/flipitservice/domain/model/entities/User.java
@@ -1,6 +1,95 @@
-package io.amonite.flipitservice.domain.model.entities;/**
+package io.amonite.flipitservice.domain.model.entities;
+
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import org.hibernate.Hibernate;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+
+/**
  * <Class description>
  *
  * @author Luís Alves
- */public class User {
+ */
+@Entity
+@Table(name = "users")
+@TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "username", nullable = false, length = 100)
+    private String username;
+
+    @Column(name = "email", nullable = true, length = 200)
+    private String email;
+
+    @Column(name = "metadata", columnDefinition = "jsonb")
+    @Type(type = "jsonb")
+    private JsonBinaryType metadata;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public JsonBinaryType getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(JsonBinaryType metadata) {
+        this.metadata = metadata;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        User user = (User) o;
+
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 562048007;
+    }
+
+    @Override
+    public String toString() {
+        //@formatter:off
+        return getClass().getSimpleName() + "(" +
+                "id = " + id + ", " +
+                "username = " + username + ", " +
+                "email = " + email + ", " +
+                "metadata = " + metadata + ")";
+        //@formatter:on
+    }
 }
